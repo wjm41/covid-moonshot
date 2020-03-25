0ff64c45c7c653f20418da761a22d46a6b412b12
William McCorkindale <wjm41@cam.ac.uk>
2020-03-25 07:41:31 +0000

    Added MPI functionality in GA-soap-mpi.py, modified slurm script in test/hpc

--- experiments/test_hpc/run_gen.sh
+++ experiments/test_hpc/run_gen.sh
@@ -11,6 +11,6 @@ r_cut=3.5
 a_sigm=0.3
 kernel=rematch
 
-python ../../GA-soap.py -csv ${init_pop} -tgt ${target1} -tgt2 ${target2} \
+python ../../GA-soap-mpi.py -csv ${init_pop} -tgt ${target1} -tgt2 ${target2} \
     -mut_rate ${mut_rate} -n_gens ${n_gens} -tgt_size=${target_size} \
     -size_stdev ${size_std} -rcut ${r_cut} -sigma ${a_sigm} -kernel ${kernel}
