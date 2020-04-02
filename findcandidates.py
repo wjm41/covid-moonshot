148eea9ccdecd1a3681608569b52b7f79c81d2d8
wjm41 <wjm41@cam.ac.uk>
2020-04-02 23:42:36 +0800

    added 3D overlap extraction of coordinates

--- findcandidates.py
+++ findcandidates.py
@@ -1,5 +1,6 @@
 """Short script for outputting covalent-noncovalent fragment pairs that overlap in space"""
 import argparse
+import os
 
 import numpy as np
 from scipy.spatial.distance import euclidean as euc_dist
@@ -21,13 +22,13 @@ def return_overlap(x1, x2, radius=0.8, n_overlap=3, make_plot=False, verbose=Fal
     """
 
     # read 2 xyz files
-    u_atoms = read_xyz(x1)[0].get_positions()
-    v_atoms = read_xyz(x2)[0].get_positions()
+    u_atoms = read_xyz(x1)[0]
+    v_atoms = read_xyz(x2)[0]
 
     # loop over atom xyz vectors, calculate euclidean distance
     dist_mat = np.empty((len(u_atoms),len(v_atoms)))
-    for i, u in enumerate(u_atoms):
-        for j,v in enumerate(v_atoms):
+    for i, u in enumerate(u_atoms.get_positions()):
+        for j,v in enumerate(v_atoms.get_positions()):
             dist_mat[i,j] = euc_dist(u, v)
 
     if make_plot:
@@ -41,14 +42,22 @@ def return_overlap(x1, x2, radius=0.8, n_overlap=3, make_plot=False, verbose=Fal
     overlap_atoms = np.where(np.less_equal(dist_mat,radius),1,0)
     num_overlap_atoms = np.sum(overlap_atoms)
 
+    overlap_atom_indices = np.argwhere(np.less_equal(dist_mat,radius)).T
+
     if num_overlap_atoms>=n_overlap: # choose n_overlap=4 for planar overlap
+        u_overlaps =  u_atoms[overlap_atom_indices[0]]
+        v_overlaps = v_atoms[overlap_atom_indices[1]]
+
+        del u_atoms[overlap_atom_indices[0]]
+        del v_atoms[overlap_atom_indices[1]]
+
         if verbose:
             print('Overlap found! Number of overlapping atoms: {}'.format(num_overlap_atoms))
-        return True
+        return True, u_atoms, v_atoms, u_overlaps, v_overlaps
     else:
         if verbose:
             print('No overlap found :(')
-        return False
+        return False, u_atoms, v_atoms, None, None
 
 def main(args):
     """
@@ -68,11 +77,52 @@ def main(args):
     n_overlaps = 0
     for u in covalent_indices:
         for v in non_covalent_indices:
-            overlap = return_overlap('data/covalent/Mpro-x'+u+'_0.xyz', 'data/non_covalent/Mpro-x'+v+'_0.xyz',
-                                     radius=args.radius, n_overlap=args.n_overlap, verbose=args.verbose)
+            overlap, u_atoms, v_atoms, u_overlaps, v_overlaps = return_overlap('data/covalent/Mpro-x'+u+'_0.xyz',
+                                                                               'data/non_covalent/Mpro-x'+v+'_0.xyz',
+                                                                               radius=args.radius,
+                                                                               n_overlap=args.n_overlap,
+                                                                               verbose=args.verbose)
             if overlap:
                 candidate_file.write(u+','+v+'\n')
                 n_overlaps+=1
+                if args.write_atoms:
+
+                    # copy original .xyz files
+                    # os.system('mkdir data/overlaps/'+u+'_'+v)
+                    os.system('cp data/covalent/Mpro-x'+u+'_0.xyz data/overlaps/'+u+'_'+v+'/'+u+'_orig.xyz')
+                    os.system('cp data/non_covalent/Mpro-x'+v+'_0.xyz data/overlaps/'+u+'_'+v+'/'+v+'_orig.xyz')
+
+                    # record pair indices
+                    f = open('data/overlaps/indices.csv', 'a')
+                    f.write(u+','+v+'\n')
+
+                    # save coordinates of post-deletion fragments
+                    f = open('data/overlaps/'+u+'_'+v+'/'+u+'_frag.xyz', 'w')
+                    f.write(str(len(u_atoms))+'\n'*2)
+                    u_symbols = u_atoms.get_chemical_symbols()
+                    u_xyz = u_atoms.get_positions()
+                    for i, atom in enumerate(u_atoms):
+                        f.write(u_symbols[i]+'\t'+str(u_xyz[i][0])+'\t'+str(u_xyz[i][1])+'\t'+str(u_xyz[i][2])+'\n')
+                    f = open('data/overlaps/' + u + '_' + v + '/' + v + '_frag.xyz', 'w')
+                    f.write(str(len(v_atoms)) + '\n' * 2)
+                    v_symbols = v_atoms.get_chemical_symbols()
+                    v_xyz = v_atoms.get_positions()
+                    for i, atom in enumerate(v_atoms):
+                        f.write(v_symbols[i] + '\t' + str(v_xyz[i][0])+'\t' + str(v_xyz[i][1]) + '\t' + str(v_xyz[i][2]) + '\n')
+
+                    # save coordinates of overlapping parts
+                    f = open('data/overlaps/'+u+'_'+v+'/'+u+'_overlap.xyz', 'w')
+                    f.write(str(len(u_overlaps))+'\n'*2)
+                    u_symbols = u_overlaps.get_chemical_symbols()
+                    u_xyz = u_overlaps.get_positions()
+                    for i, atom in enumerate(u_overlaps):
+                        f.write(u_symbols[i]+'\t'+str(u_xyz[i][0])+'\t'+str(u_xyz[i][1])+'\t'+str(u_xyz[i][2])+'\n')
+                    f = open('data/overlaps/' + u + '_' + v + '/' + v + '_overlap.xyz', 'w')
+                    f.write(str(len(v_overlaps)) + '\n' * 2)
+                    v_symbols = v_overlaps.get_chemical_symbols()
+                    v_xyz = v_overlaps.get_positions()
+                    for i, atom in enumerate(v_overlaps):
+                        f.write(v_symbols[i] + '\t' + str(v_xyz[i][0])+'\t' + str(v_xyz[i][1]) + '\t' + str(v_xyz[i][2]) + '\n')
 
     print('Analysis complete: {} overlapping fragments found. Results saved in: {}'.format(n_overlaps, args.out))
 
@@ -88,6 +138,8 @@ if __name__ == '__main__':
                         help='radius threshold for determining overlap of fragment atoms')
     parser.add_argument('-n_overlap',type=int, default=4,
                         help='minimum number of atom pairs within threshold for fragment pair to count as overlapping')
+    parser.add_argument('-write_atoms', type=bool, default=False,
+                        help='whether to write fragment .xyz files with overlapping structure deleted.')
     parser.add_argument('-verbose', type=bool, default=False,
                         help='whether to print extra statements')
     args = parser.parse_args()
