2593039b4ac904e770a9385b942b1caee6bdffa8
William McCorkindale <wjm41@cam.ac.uk>
2020-04-02 20:16:35 +0100

    updated 0692 folders and generate_smiles.sh

--- data/overlaps/generate_smiles.sh
+++ data/overlaps/generate_smiles.sh
@@ -1,7 +1,7 @@
-for i in */;
+for i in *_updated/;
 do
-babel -ixyz ${i}*.xyz -ocan ${i}smiles.can
-cat ${i}smiles.can >> smiles.can
-echo \ >> smiles.can
+babel -ixyz ${i}*.xyz -ocan ${i}smiles_updated.can
+cat ${i}smiles_updated.can >> smiles_updated.can
+echo \ >> smiles_updated.can
 done
 
