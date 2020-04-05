775424a6391ecf0ca673eee69a5e1435b6c4763c
wjm41 <wjm41@cam.ac.uk>
2020-04-05 10:53:27 +0800

    added generic scaffolds

--- scaffold_cluster.py
+++ scaffold_cluster.py
@@ -1,8 +1,8 @@
 import argparse
 
 import pandas as pd
-from rdkit.Chem import Draw, MolFromSmiles
-from rdkit.Chem.Scaffolds.MurckoScaffold import MurckoScaffoldSmilesFromSmiles
+from rdkit.Chem import Draw, MolFromSmiles, MolToSmiles
+from rdkit.Chem.Scaffolds.MurckoScaffold import MurckoScaffoldSmilesFromSmiles, MakeScaffoldGeneric
 
 def str2bool(v):
     if isinstance(v, bool):
@@ -23,17 +23,27 @@ def main(args):
     # Create dictionary of key: scaffold and value: list of smiles
     scaffold_dict = {}
     scaff_list = []
+    scaffold_generic_dict = {}
+    scaff_generic_list = []
     for i, smiles in enumerate(data['SMILES'].values):
         scaffold = MurckoScaffoldSmilesFromSmiles(smiles)
+        scaffold_generic = MolToSmiles(MakeScaffoldGeneric(MolFromSmiles(scaffold)))
+        #print(scaffold)
+        #print(scaffold_generic)
         scaff_list.append(scaffold)
+        scaff_generic_list.append(scaffold_generic)
         if scaffold not in scaffold_dict.keys():
             scaffold_dict[scaffold] = [smiles]
         else:
-            smiles_list = scaffold_dict[scaffold]
             scaffold_dict[scaffold].append(smiles)
+        if scaffold_generic not in scaffold_generic_dict.keys():
+            scaffold_generic_dict[scaffold_generic] = [smiles]
+        else:
+            scaffold_generic_dict[scaffold_generic].append(smiles)
         #print(scaffold_dict)
     data['scaffold'] = scaff_list
-    data[['SMILES','TITLE','dockvalent_id','scaffold']].to_csv('data/unique_docked_smiles_by_scaffold.csv', index=False)
+    data['scaffold_generic'] = scaff_generic_list
+    data[['SMILES','TITLE','dockvalent_id','scaffold','scaffold_generic']].to_csv('data/unique_docked_smiles_by_scaffold.csv', index=False)
 
     scaff_list = []
     print('\nNumber of distinct scaffolds: {}'.format(len(scaffold_dict)))
@@ -49,6 +59,21 @@ def main(args):
     img = Draw.MolsToGridImage(scaff_mols, molsPerRow=5, subImgSize=(200,200),
                                legends=['counts = {}'.format(len(scaffold_dict[scaffold])) for scaffold in scaff_list])
     img.save('data/scaff_grid.png')
+
+    scaff_generic_list = []
+    print('\nNumber of distinct generic scaffolds: {}'.format(len(scaffold_generic_dict)))
+    n_scaff = 0
+    for scaffold_gen in sorted(scaffold_generic_dict, key=lambda k: len(scaffold_generic_dict[k]), reverse=True):
+        n_scaff += 1
+        if n_scaff<=20:
+            print('#{} generic scaffold: {}, size: {}'.format(n_scaff, scaffold_gen, len(scaffold_generic_dict[scaffold_gen])))
+            scaff_generic_list.append(scaffold_gen)
+        else:
+            break
+    scaffgen_mols = [MolFromSmiles(scaffold) for scaffold in scaff_generic_list]
+    img = Draw.MolsToGridImage(scaffgen_mols, molsPerRow=5, subImgSize=(200,200),
+                               legends=['counts = {}'.format(len(scaffold_generic_dict[scaffold])) for scaffold in scaff_generic_list])
+    img.save('data/scaff_generic_grid.png')
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
